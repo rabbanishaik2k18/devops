17cbfc9a39eb02d735bd8468345b8e9e4c148477
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-08 11:44:38 +0530

    conversation

---
+++ 04-varibles.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+
+echo "$PERSON1: Hello $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good.and you"
+echo "$PERSON1: I am fine"
