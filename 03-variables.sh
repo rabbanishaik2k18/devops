20673b17a1f756e6f325cb7b545e2162f41b3846
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-08 11:43:13 +0530

    conversation

---
+++ 03-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=RAMESH
+PERSON2=Suresh
+
+
+echo "$PERSON1: Hello $PERSON2"
+echo "$PERSON2: Hello $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: I am good.and you"
+echo "$PERSON1: I am fine"
