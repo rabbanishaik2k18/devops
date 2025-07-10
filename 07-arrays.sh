9a943d7825fe48e2a81aec78e40082ee80af20b5
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-10 11:26:32 +0530

    datatype

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,8 +2,8 @@
 
 MOVIES=$(("pushpa" "rrr" "Devara"))
 
-echo "first movie : $MOVIES[0]"
-echo "second movie : $MOVIES[1]"
-echo "third movie : $MOVIES[2]"
+echo "first movie : $MOVIES{[0]}"
+echo "second movie : $MOVIES{[1]}"
+echo "third movie : $MOVIES{[2]}"
 
-echo " all movies :$MOVIES[@]"
+echo " all movies :$MOVIES{[@]}"
