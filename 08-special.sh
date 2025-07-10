493259c38c669f050ce5da6ed44b61e1a9a24d90
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-10 11:38:05 +0530

    datatype

---
+++ 08-special.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+all variables : $@
+all numbers : $#
+script name : $0
+script current directory: $PWD
+home directory for running script : $HOME
+which user is running script : $USER
+process id for current script : $$
+process id for last command in back groun : $!
