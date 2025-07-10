d58b97e218f918237477bcbca94e292e3daf95d0
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-10 10:47:29 +0530

    datatype

--- 03-variables.sh
+++ 03-variables.sh
@@ -4,7 +4,7 @@ PERSON1=RAMESH
 PERSON2=Suresh
 
 
-echo "Rahim: Hello Robert"
+echo "$PERSON1: $PERSON2"
 echo "$PERSON2: Hello $PERSON1"
 echo "$PERSON1: How are you $PERSON2"
 echo "$PERSON2: I am good.and you"
