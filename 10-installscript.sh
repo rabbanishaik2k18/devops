e17ae08a4f2d6a961e607b3d0d9e14ee312252d1
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-11 12:14:18 +0530

    condiotion

--- 10-installscript.sh
+++ 10-installscript.sh
@@ -10,4 +10,19 @@ fi
 
 dnf install mysql -y
 
+if [ $? -ne 0 ]
+then
+    echo "installing mysql is failure"
+else
+    echo "installing mysql is success"
+fi
+
+
 dnf install git -y
+
+if [ $? -ne 0 ]
+then
+    echo "installing git is failure"
+else
+    echo "installing git is success"
+fi
