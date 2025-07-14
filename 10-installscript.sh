f6da5dee3e0e4a1c9793aedccba3acb5b074b297
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-14 12:33:48 +0530

    all directorires

--- 10-installscript.sh
+++ 10-installscript.sh
@@ -8,6 +8,11 @@ then
     exit 1
 fi
 
+dnf list installed mysql
+
+if [ $? -ne 0 ]
+
+then
 dnf install mysql -y
 if [ $? -ne 0 ]
 then
@@ -17,6 +22,13 @@ else
 echo "installing mysql is success"
 fi
 
+else
+echo "mysql is already installed"
+fi
+
+dnf list installed git
+if [ $? -ne 0 ]
+then
 dnf install git -y
 
 if [ $? -ne 0 ]
@@ -25,4 +37,7 @@ echo "install git is failue"
 exit 1
 else
 echo "installing git is success"
+fi
+else
+echo "git is already installed"
 fi
