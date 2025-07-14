453287a7a142545e42ad469473f68647fa0e4704
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-14 12:03:58 +0530

    all directorires

--- 10-installscript.sh
+++ 10-installscript.sh
@@ -1,36 +1,9 @@
 #!/bin/bash
 
-USERID=$(id -u)
+USEDID=$(id -u)
 
-if [ $USERID -ne 0 ]
-then
-    echo "ERROR : You must have root access to install this scriipt"
-    exit 1
-fi
+if [ $USERID -ne 0]
+then 
+echo "Error: you must have super prvillages"
 
-dnf list installed mysql
-
-if [ $? -ne 0 ]
-then
-        dnf install mysql -y
-
-        if [ $? -ne 0 ]
-        then
-           echo "installing mysql is failure"
-        else
-            echo "installing mysql is success"
-        fi
-
-else
-   echo "mysql already installed"
-fi
-
-
-dnf install git -y
-
-if [ $? -ne 0 ]
-then
-    echo "installing git is failure"
-else
-    echo "installing git is success"
-fi
+dnf install mysql1 -y
