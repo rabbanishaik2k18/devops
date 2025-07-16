c80c077b2a4496d0ff070d75ea930db05881d15b
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-16 13:08:50 +0530

    all files

--- 15-script.sh
+++ 15-script.sh
@@ -33,22 +33,15 @@ then
     exit 1
 fi
 
-dnf list installed mysql &>>$LOG_FILE_NAME
-
-if [ $? -ne 0 ]
-then
-        dnf install mysql -y &>>$LOG_FILE_NAME
-        VALIDATE $? "Installing mysql sucess" 
-
-else
-echo -e "mysql is already $Y installed" 
-fi
-
-dnf list installed git &>>$LOG_FILE_NAME
-if [ $? -ne 0 ]
-then
-dnf install git -y &>>$LOG_FILE_NAME
-VALIDATE $? "Installing git"
-else
-echo -e "git is already $Y installed" 
-fi
+for package in $@
+do
+  dnf list install $package &>>LOG_FILE_NAME
+  if [ $? -ne 0]
+  then
+  dnf install $package &>>LOG_FILE_NAME
+  VALIDATE $? "Installing mysql"
+  else
+  echo -e "$package already $Y installed" &>>LOG_FILE_NAME
+  fi
+done
+  
