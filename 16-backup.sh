cdd26dc98601e091c4fdb5ab78f5e33ff24bbcfd
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-07-23 20:58:20 +0530

    deletelogs

--- 16-backup.sh
+++ 16-backup.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 
 SOURCE_DIR="/home/ec2-user/app-logs/"
 
-LOGS_FOLDER="/var/log/shell-script-log"
+LOGS_FOLDER="/var/log/shell-script-logs"
 LOGS_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP"
