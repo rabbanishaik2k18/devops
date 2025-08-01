7a1acb1b2d9dcb6f291eb5217f00ff02109f1942
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-08-01 17:46:47 +0530

    backupfiles

--- 18-zipbackups.sh
+++ 18-zipbackups.sh
@@ -11,7 +11,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/Shell-script-log"
+LOGS_FOLDER="/home/ec2-user/app-logs"
 LOGS_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP"
@@ -22,4 +22,5 @@ USAGE(){
     echo " usage :: sh 18-zipbackups.sh <SOUR_DIR> <DEST_DIR> <DAYS>"
 }
 
+mkdir -p /home/ec2-user/app-logs
 echo "script installed date: $TIMESTAMP" &>>$LOG_FILE_NAME
