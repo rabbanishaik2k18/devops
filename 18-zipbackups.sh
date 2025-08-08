a721024c118b78c3252b8b36d43bd4a284f7a1de
rabbanishaik <rabbanishaik2k18@gmail.com>
2025-08-08 09:09:44 +0530

    all files

--- 18-zipbackups.sh
+++ 18-zipbackups.sh
@@ -2,45 +2,32 @@
 
 USERID=$(id -u)
 
-SOUR_DIR=$1
-DEST_DIR=$2
-DAYS=${3:-14}
-
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/home/ec2-user/app-logs"
+SOURCE_DIR=$1
+DEST_DIR=$2
+DAYS=${3:-14}
+
+LOGS_FOLDER="/var/log/Shell-script-log"
 LOGS_FILE=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date "+%Y-%m-%d %H:%M:%S")
 LOG_FILE_NAME="$LOGS_FOLDER/$LOGS_FILE-$TIMESTAMP"
 
-
-
 USAGE(){
-    echo -e " usage :: sh 18-zipbackups.sh <SOUR_DIR> <DEST_DIR> <DAYS>"
-    
+    echo "usage :: sh 18-zipbackups.sh <SOURCE_DIR> <DEST_DIR>"
+    exit 1
 }
 
-mkdir -p /home/ec2-user/app-logs
-echo "script installed date: $TIMESTAMP" &>>$LOG_FILE_NAME
-
 if [ $# -lt 2 ]
-then
+then 
 USAGE
 fi
-if [ ! -d $SOUR_DIR ]
-then
-echo "$SOUR_DIR is does not exist"
-exit 1
-fi
-if [ ! -d $DEST_DIR ]
-then
-echo "$DEST_DIR is does not exist"
-exit 1
-fi
-find $SOUR_DIR -name "*.log" -mtime +$DAYS
-FILES=$(find $SOUR_DIR -name "*.log" -mtime +$DAYS)
 
-echo "files are : $FILES"
+
+echo "script installed date: $TIMESTAMP" &>>$LOG_FILE_NAME
+
+
+  
